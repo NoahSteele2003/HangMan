8e6bdf91abca61056a38dd8bc65ccc0dddcccaeb
Unknown <noah.steele.2021@student.lhschools.org>
2018-11-08 08:37:38 -0500

    Added cards up to loser and winner comment.

--- src/HangMan.java
+++ src/HangMan.java
@@ -7,28 +7,44 @@ public class HangMan{
         String pickedPhrase;
         String hiddenPhrase;
         String pickedLetter;
-        int position;
+        int position, tries;
+        tries = 15;
 
 
         System.out.println("Welcome to hangman! To play, one player types a phrase to use for the hangman.\nThen, the other player picks letters that may be in the phrase, and then they will be told if they are correct or not.\nKeep guessing letters until you guess the phrase or you run out of tries.\n");
         System.out.println("Player 1, type a phrase that will be used for the game of hangman.\n");
-        pickedPhrase = keyboard.nextLine();
+        pickedPhrase = keyboard.nextLine().toLowerCase();
 
         hiddenPhrase = phraseToDash(pickedPhrase);
 
-        System.out.println("Player 2, pick a letter that might be in the phrase.");
-        pickedLetter = keyboard.nextLine();
-
-
-        if(pickedPhrase.contains(pickedLetter)){
-            while(pickedPhrase.contains(pickedLetter)){
-                position = pickedPhrase.indexOf(pickedLetter);
-                //System.out.println(position);
-                hiddenPhrase = hiddenPhrase.substring(0,position)+pickedLetter+hiddenPhrase.substring(position+1,pickedPhrase.length());
-                pickedPhrase = pickedPhrase.replaceFirst(pickedLetter," ");
+        for(int x=0;x<15;x++) {
+            if(!hiddenPhrase.contains("-")){
+                System.out.println("You guessed the phrase! You win!");
+                break;
             }
-            System.out.println(hiddenPhrase);
+            System.out.println(tries+" tries left");
+            System.out.println("\nPlayer 2, pick a letter that might be in the phrase.");
+            pickedLetter = keyboard.nextLine().toLowerCase();
+            if (pickedPhrase.contains(pickedLetter)) {
+                x--;
+                while (pickedPhrase.contains(pickedLetter)) {
+                    position = pickedPhrase.indexOf(pickedLetter);
+                    //System.out.println(position);
+                    hiddenPhrase = hiddenPhrase.substring(0, position) + pickedLetter + hiddenPhrase.substring(position + 1, pickedPhrase.length());
+                    pickedPhrase = pickedPhrase.replaceFirst(pickedLetter, " ");
+                }
+                System.out.println(hiddenPhrase);
+            }
+            else if(!pickedPhrase.contains(pickedLetter)&&x<14){
+                System.out.println("Sorry, try a different letter.\n");
+                System.out.println(hiddenPhrase);
+                tries--;
+            }
+            if(x==14){
+                System.out.println("Sorry, you ran out of tries, the correct phrase was \""+pickedPhrase+"\"");
+            }
         }
+
 
 
 
